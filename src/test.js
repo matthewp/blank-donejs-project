1238c62a08e333dc40ecbb092281c80635214ecc
Matthew Phillips <matthew@matthewphillips.info>
2017-04-12 12:53:33 -0400

    initial commit

---
+++ src/test.js
@@ -0,0 +1,16 @@
+import F from 'funcunit';
+import QUnit from 'steal-qunit';
+
+import 'donejs-chat/models/test';
+
+F.attach(QUnit);
+
+QUnit.module('donejs-chat functional smoke test', {
+  beforeEach() {
+    F.open('./development.html');
+  }
+});
+
+QUnit.test('donejs-chat main page shows up', function() {
+  F('title').text('donejs-chat', 'Title is set');
+});
